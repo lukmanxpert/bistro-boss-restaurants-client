575a47c286c08b8d17b8970ccc1f23e7bdb79841
lukmanxpert <lukmanmiah2004@gmail.com>
2025-01-01 02:24:02 +0600

    added shared section title

---
+++ src/components/shared/SectionTitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SectionTitle = ({ title, subTitle }) => {
+    return (
+        <div className='max-w-lg my-8 mx-auto text-center'>
+            <p className='text-[#D99904] italic text-lg my-2'>{subTitle}</p>
+            <hr className='border-t-4' />
+            <p className='my-2 text-4xl'>{title}</p>
+        </div>
+    );
+};
+
+export default SectionTitle;
--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../../../components/Banner/Banner';
 import Category from '../../../components/category/Category';
+import SectionTitle from '../../../components/shared/SectionTitle';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <SectionTitle subTitle={"---From 11:00am to 10:00pm---"} title={"ORDER ONLINE"}></SectionTitle>
             <Category></Category>
         </div>
     );
