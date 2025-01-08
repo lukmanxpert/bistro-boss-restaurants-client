83cf70c8645f2e773c5fa2e21e424cca939a9a0d
lukmanxpert <lukmanmiah2004@gmail.com>
2025-01-09 02:41:10 +0600

    added price inside card

--- src/pages/our-shop/Card.jsx
+++ src/pages/our-shop/Card.jsx
@@ -4,8 +4,9 @@ const Card = ({ menu }) => {
     return (
         <div className="card rounded-none bg-base-100 shadow-xl">
             <figure>
-                <img className='w-full'
+                <img className='w-full relative'
                     src={menu.image} />
+                    <span className='absolute top-0 right-5 text-white font-bold bg-black py-2 px-4 my-2 rounded-md'>${menu.price}</span>
             </figure>
             <div className="text-center">
                 <h2 className="text-2xl font-bold my-2">{menu.name}</h2>
